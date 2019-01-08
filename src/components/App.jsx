e45e97931d11b8a0889e81a211410a57ef9ec89a
Edward Leslie <edwardleslie09@gmail.com>
2019-01-08 17:31:06 -0500

    changed .js to .jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import Header from "./Header";
-import Inventory from "./Inventory";
-import Order from "./Order";
+import Header from "./Header.jsx";
+import Inventory from "./Inventory.jsx";
+import Order from "./Order.jsx";
 
 class App extends Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
         <Inventory />
